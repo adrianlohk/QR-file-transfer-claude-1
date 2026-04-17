bcf3542436bee2b74ba45c96423f529b70943b6d
Adrianlo <adrianlohk@gmail.com>
2026-04-18 00:24:59 +0800

    Fix Vercel deployment configuration

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.get('/download', (req, res) => {
   res.download(fileData.path, fileData.originalName);
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
